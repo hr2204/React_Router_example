5d574f57f122c91d36934964942b45d0ba45d947
hr2204 <hr2204@hotmail.com>
2017-05-04 17:07:24 -0700

    better than nothing...

---
+++ src/components/posts_index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by rhe on 5/4/17.
+ */
+
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render(){
+        return (
+            <div>
+                Posts Index
+            </div>
+        )
+    }
+}
+
+export default PostsIndex
