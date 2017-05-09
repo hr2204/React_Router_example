c470b03f2f13ccd533b19ac61d6b40cd2f4d7b98
hr2204 <hr2204@hotmail.com>
2017-05-08 17:37:37 -0700

    update lik

---
+++ src/components/posts_new.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rhe on 5/8/17.
+ */
+
+
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render(){
+        return (
+            <div>
+                PostNew!
+            </div>
+        )
+    }
+}
+
+export default PostsNew;
