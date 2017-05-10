c7d61e2d817585c6d6981dff45fc56ec7f84904d
hr2204 <hr2204@hotmail.com>
2017-05-09 17:01:31 -0700

    lec 134

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,15 +4,43 @@
 
 
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
 
 class PostsNew extends Component {
+    renderField(field) {
+        return (
+          <div className="form-group">
+              <label>{}</label>
+              <input
+                  className="form-control"
+                  type="text"
+                  {...field.input}
+              />
+          </div>
+        );
+    }
+
+
+
     render(){
         return (
-            <div>
-                PostNew!
-            </div>
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    name="tags"
+                    component={this.renderField}
+                />
+            </form>
         )
     }
 }
 
-export default PostsNew;
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
+
