a9d95e3993439c6b4a26a01a38731976e7c1d16d
hr2204 <hr2204@hotmail.com>
2017-05-10 17:54:38 -0700

    lec 137

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,36 +11,71 @@ class PostsNew extends Component {
     renderField(field) {
         return (
           <div className="form-group">
-              <label>{}</label>
+              <label>{field.label}</label>
               <input
                   className="form-control"
                   type="text"
                   {...field.input}
               />
+              {field.meta.error}
           </div>
         );
     }
 
+    onSubmit(values){
+        console.log(values);
+    }
 
 
     render(){
+        const  { handleSubmit } = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
+                    label="Title For Post"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         )
     }
 }
 
+function validate(values) {
+    //console.log(values) -> {title: xxx, categories:xxx,content:xxx}
+    const errors = {};
+
+    //validate the input from 'values'
+    if (!values.title ){
+
+        errors.title = "Enter a title ";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content please";
+    }
+    //if errors is empty, the form is fine to submit
+    //if errors has *any* properties, redux form assumes form is invalid
+    return errors;
+
+}
 
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
 
